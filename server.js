fc7b50731e1e30213953ec418ffef1a141b6f3ac
Carla Paiano <machadogcarla@gmail.com>
2020-03-27 17:49:41 -0300

    Segunda versao completa

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express'); //importando o express
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const cors = require('cors');
 
 //Iniciando o app
 const app = express();
 app.use(express.json()); //permite que envio dados da minha aplicacao em formato de json
-
+app.use(cors()); //dominio de acesso, conf de seguranca
 
 //Iniciando o DB --> nodeapi e o nome do banco de dados
 mongoose.connect("mongodb://localhost:27017/nodeapi",
